0eb285b9e62e7af5f8a1503bdca874b74eb02a20
tsdnhts <jyjangtop@hanmail.net>
2024-01-23 12:30:38 +0900

    작업 내용 반영(컨트롤러, 예외처리, 서비스, 서비스 임플 틀만 구현) 엔티티, 레포지토리, 서비스 임플 추가 구현 필요함

--- backoffice/src/main/kotlin/com/example/backoffice/store/service/StoreService.kt
+++ backoffice/src/main/kotlin/com/example/backoffice/store/service/StoreService.kt
@@ -1,4 +1,20 @@
 package com.example.backoffice.store.service
 
+import com.example.backoffice.store.dto.CreateStoreRequest
+import com.example.backoffice.store.dto.StoreResponse
+import com.example.backoffice.store.dto.UpdateStoreRequest
+
+
 interface StoreService {
+
+    fun getAllStoreList() : List<StoreResponse>
+
+    fun getStoreById(storeId: Long) : StoreResponse
+
+    fun createStore(request : CreateStoreRequest) : StoreResponse
+
+    fun updateStore(storeId : Long, request: UpdateStoreRequest) : StoreResponse
+
+    fun deleteStore(storeId: Long)
+
 }
