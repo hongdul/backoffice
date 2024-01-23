e32a0268410c5505b3fb23ead4a99cba4193bb00
tsdnhts <jyjangtop@hanmail.net>
2024-01-23 14:53:54 +0900

    chore: 프로그램 구현을 위해 h2 database gradle에 추가 StoreEntity.kt 임시 주석 처리

--- backoffice/src/main/kotlin/com/example/backoffice/store/model/StoreEntity.kt
+++ backoffice/src/main/kotlin/com/example/backoffice/store/model/StoreEntity.kt
@@ -1,10 +1,10 @@
-package com.example.backoffice.store.model
-
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
-
-@Entity
-@Table
-class StoreEntity {
-
-}
+//package com.example.backoffice.store.model
+//
+//import jakarta.persistence.Entity
+//import jakarta.persistence.Table
+//
+//@Entity
+//@Table
+//class StoreEntity {
+//
+//}
