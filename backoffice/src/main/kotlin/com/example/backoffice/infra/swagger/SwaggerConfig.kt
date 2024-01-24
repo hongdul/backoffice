6e3c6c8dee63ff6546f943c9254fdeef0c24ac9e
hongdul <kkw0426@naver.com>
2024-01-24 11:21:13 +0900

    Move: SwaggerConfig 클래스 domain 하위에서 상위풀더로 이동

--- backoffice/src/main/kotlin/com/example/backoffice/infra/swagger/SwaggerConfig.kt
+++ backoffice/src/main/kotlin/com/example/backoffice/infra/swagger/SwaggerConfig.kt
@@ -1,4 +1,4 @@
-package com.example.backoffice.domain.infra.swagger
+package com.example.backoffice.infra.swagger
 
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
