10686b253562977780826f32c044b8d537db8d11
hongdul <kkw0426@naver.com>
2024-01-24 11:19:53 +0900

    Feat: MenuResponse에 from 동반객체 추가
    - Menu 객체 인자로 받아 MenuResponse로 반환하는 함수

--- backoffice/src/main/kotlin/com/example/backoffice/domain/menu/dto/MenuResponse.kt
+++ backoffice/src/main/kotlin/com/example/backoffice/domain/menu/dto/MenuResponse.kt
@@ -1,12 +1,26 @@
 package com.example.backoffice.domain.menu.dto
 
+import com.example.backoffice.domain.menu.model.Menu
 import com.example.backoffice.domain.menu.model.MenuStatus
 
 data class MenuResponse(
     val id: Long?,
+    val storeId: Long?,
     val name: String,
     val description: String,
-    val storeId: Long?,
-    val status: MenuStatus,
     val price: Long,
-)
+    val status: MenuStatus,
+) {
+    companion object {
+        fun from(menu: Menu): MenuResponse {
+            return MenuResponse(
+                id = menu.id!!,
+                storeId = menu.store.id!!,
+                name = menu.name,
+                description = menu.description,
+                price = menu.price,
+                status = menu.status
+            )
+        }
+    }
+}
