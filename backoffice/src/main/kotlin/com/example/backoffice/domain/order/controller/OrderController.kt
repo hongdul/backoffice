eae5c209e5b7a73ce4fbb87e8769bcd353229c78
hongdul <kkw0426@naver.com>
2024-01-28 01:45:29 +0900

    Style: menuInCart 의 @Operation summary 변경
    - 장바구니 -> 장바구니 담기

--- backoffice/src/main/kotlin/com/example/backoffice/domain/order/controller/OrderController.kt
+++ backoffice/src/main/kotlin/com/example/backoffice/domain/order/controller/OrderController.kt
@@ -1,14 +1,15 @@
 package com.example.backoffice.domain.order.controller
 
-import com.example.backoffice.domain.menu.dto.MenuArguments
 import com.example.backoffice.domain.order.dto.CartDto
 import com.example.backoffice.domain.order.dto.CartRequest
+import com.example.backoffice.domain.order.dto.OrderMapDto
 import com.example.backoffice.domain.order.service.OrderService
 import com.example.backoffice.infra.security.UserPrincipal
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -23,7 +24,7 @@ class OrderController(
     private val orderService: OrderService
 ) {
 
-    @Operation(summary = "장바구니", description = "장바구니에 메뉴담기")
+    @Operation(summary = "장바구니 담기", description = "장바구니에 메뉴담기")
     @PostMapping
     fun menuInCart(
         @RequestParam menuId: Long,
@@ -37,7 +38,7 @@ class OrderController(
     }
 
     @Operation(summary = "장바구니 조회")
-    @GetMapping
+    @GetMapping("/cart")
     fun getCart(
         @AuthenticationPrincipal user: UserPrincipal
     ): ResponseEntity<List<CartDto>> {
@@ -47,11 +48,20 @@ class OrderController(
             .body(carts)
     }
 
-    @Operation(summary = "주문하기", description = "장바구니에 있는 메뉴를 주문합니다. ")
-    @PutMapping
-    fun order(
+    @Operation(summary = "장바구니 비우기")
+    @DeleteMapping("/cart/delete")
+    fun deleteCart(
         @AuthenticationPrincipal user: UserPrincipal
-    ) {
+    ): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(orderService.deleteCart(user))
+    }
+
+
+    @Operation(summary = "주문하기", description = "장바구니에 있는 메뉴를 주문합니다. ")
+    @PutMapping("/order")
+    fun order(@AuthenticationPrincipal user: UserPrincipal) {
         orderService.order(user)
     }
 
